189f984fd357ac81ce9bbe923a06e037adc485d1
hackest <25418711@qq.com>
2021-11-19 15:34:06 +0800

    解决编译失败

--- library/src/main/java/com/dueeeke/videoplayer/util/StorageUtil.java
+++ library/src/main/java/com/dueeeke/videoplayer/util/StorageUtil.java
@@ -3,9 +3,6 @@ package com.dueeeke.videoplayer.util;
 import android.content.Context;
 import android.os.Environment;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
 import java.io.File;
 
 import static android.os.Environment.MEDIA_MOUNTED;
@@ -20,7 +17,6 @@ import static android.os.Environment.MEDIA_MOUNTED;
  */
 public class StorageUtil {
 
-    private static final Logger LOG = LoggerFactory.getLogger("StorageUtil");
     private static final String INDIVIDUAL_DIR_NAME = "video-cache";
 
     /**
@@ -63,7 +59,6 @@ public class StorageUtil {
         }
         if (appCacheDir == null) {
             String cacheDirPath = context.getFilesDir().getPath() + context.getPackageName() + "/cache/";
-            LOG.warn("Can't define system cache directory! '" + cacheDirPath + "%s' will be used.");
             appCacheDir = new File(cacheDirPath);
         }
         return appCacheDir;
@@ -74,7 +69,6 @@ public class StorageUtil {
         File appCacheDir = new File(new File(dataDir, context.getPackageName()), "cache");
         if (!appCacheDir.exists()) {
             if (!appCacheDir.mkdirs()) {
-                LOG.warn("Unable to create external cache directory");
                 return null;
             }
         }
